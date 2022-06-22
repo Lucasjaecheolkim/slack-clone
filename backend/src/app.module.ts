45e6ec799559f7f2a211cd967fd681d80e0e7229
Lucasjaecheolkim <jaecheol.kim@flitto.com>
2022-06-22 11:46:49 +0900

    pjt start

---
+++ backend/src/app.module.ts
@@ -0,0 +1,34 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
+
+@Module({
+    // forRoot: 설정을 넣어주기 위해서...
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true }), 
+      UsersModule,
+      WorkspacesModule,
+      ChannelsModule,
+      DmsModule],
+    controllers: [AppController],
+    providers: [AppService],
+
+    // 원형
+    // providers: [
+    //   {
+    //     provide: AppService, // 고유한키
+    //     useClass: AppService,
+    //   },
+    // ],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
